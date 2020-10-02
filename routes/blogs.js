0d6a1d12b3ee63702875569d4ca5de6544659a0f
adityagaddhyan <adityagaddhyan@gmail.com>
2020-10-02 22:28:35 +0530

    debugged

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,12 +2,23 @@ var express=require("express");
 var router=express.Router();
 var Blog=require("../models/blogModel");
 var mongoose=require("mongoose");
-
+mongoose.set('useFindAndModify', false);
 //NEW ROUTE
 router.get("/new", (req, res) => {
     res.render("new");
 });
 //CREATE
+
+router.get("/", (req, res) => {
+    Blog.find({}).limit(10).exec(function (err, result) {
+        if (err) {
+            console.log("error");
+        }
+        else {
+            res.render("index", { blogs: result });
+        }
+    });
+});
 router.post("/", (req, res) => {
     console.log("POST"); req.body
     var Title1 = req.sanitize(req.body.title);
@@ -21,26 +32,17 @@ router.post("/", (req, res) => {
         }
         else {
             console.log("CREATED new blog");
-            res.redirect("//" + u._id);
+            console.log(u._id);
+            res.redirect("/blogs/" + u._id);
         }
     })
-});
-router.get("/", (req, res) => {
-    Blog.find({}).limit(10).exec(function (err, result) {
-        if (err) {
-            console.log("error");
-        }
-        else {
-            res.render("index", { blogs: result });
-        }
-    });
 });
 //SHOW ONE
 router.get("/:id", (req, res) => {
     Blog.findById(req.params.id, (err, found) => {
         if (err) {
             console.log("error in finding blog");
-            res.redirect('/');
+            res.redirect('/blogs');
         }
         else {
             console.log("VIEWING " + req.params.id)
@@ -54,7 +56,7 @@ router.get("/:id/edit", (req, res) => {
     Blog.findById(ID, (err, found) => {
         if (err) {
             console.log("cannot find the post");
-            res.render("/");
+            res.redirect("/blogs");
         }
         else {
             console.log("post EDIT page")
@@ -68,11 +70,11 @@ router.put("/:id", (req, res) => {
     Blog.findByIdAndUpdate(req.sanitize(req.params.id), { $set: req.body.updates }, { returnOriginal: false }, (err, updated) => {
         if (err) {
             console.log("EDIT failed");
-            res.redirect("/");
+            res.redirect("/blogs");
         }
         else {
             console.log("EDIT success");
-            res.redirect("//" + req.params.id);
+            res.redirect("/blogs/" + req.params.id);
         }
     });
 })
@@ -81,11 +83,11 @@ router.delete("/:id", (req, res) => {
     Blog.findByIdAndDelete(req.params.id, err => {
         if (err) {
             console.log("unable to DELETE");
-            res.redirect('/');
+            res.redirect('/blogs');
         }
         else {
             console.log("succesfully DELETED");
-            res.redirect('/')
+            res.redirect('/blogs')
         }
     });
 });
